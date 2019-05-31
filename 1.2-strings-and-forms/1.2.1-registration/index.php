cbd966e19dc10e71043d9d8fe22073a4d4a3b0b7
sheerazspb <sheerazspb@gmail.com>
2019-05-31 20:29:41 +0300

    fix 1.2-strings-and-forms/1.2.1-registration/index.php file

--- 1.2-strings-and-forms/1.2.1-registration/index.php
+++ 1.2-strings-and-forms/1.2.1-registration/index.php
@@ -1,21 +1,43 @@
 <?php
 $login = $_POST['login'];
-$password = $_POST['password'];
+$pass = $_POST['password'];
 $email = $_POST['email'];
 $firstName = $_POST['firstName'];
 $lastName = $_POST['lastName'];
 $middleName = $_POST['middleName'];
-$code = str_replace(' ',  '', $_POST['code']);
-$validCode = 'code';
-
-if (preg_match('/\@|\;|\?|\/|\:|\*/' , $login) === 1){
-    echo "Логин содержит недопустимые символы - @ / * ? , ; : <br>";
+$code = $_POST['code'];
+$codeWord = 'code';
+$isCorrect = true;
+$patternLogin = '/\W/';
+if (preg_match($patternLogin, $login)) {
+    echo 'Поле логин не должно содержать символы @/*?,;:. <br>';
+    $isCorrect = false;
 }
-
-if (strlen($password) < 8) {
-    echo "Длина пароля должна быть минимум 8 символов <br>";
+if (strlen($pass) < 8) {
+    echo 'Длина пароля должна быть минимум 8 символов <br>';
+    $isCorrect = false;
 }
-
-if ($validCode !== strtolower($code)) {
-    echo "Кодовое слово неверно <br>";
-} 
+$patternEmail = '/\A[^@]+@([^@\.]+\.)+[^@\.]+\z/';
+if (!preg_match($patternEmail, $email)) {
+    echo 'Почта должна быть формата почта@домен.доменнаязона <br>';
+    $isCorrect = false;
+}
+if (strlen($firstName) === 0) {
+    echo 'Поле Имя не может быть пустым <br>';
+    $isCorrect = false;
+}
+if (strlen($lastName) === 0) {
+    echo 'Поле Фамилия не может быть пустым <br>';
+    $isCorrect = false;
+}
+if (strlen($middleName) === 0) {
+    echo 'Поле Отчество не может быть пустым <br>';
+    $isCorrect = false;
+}
+if ($codeWord !== trim(strtolower($code))) {
+    echo 'Вы неверно указали Кодовое слово <br>';
+    $isCorrect = false;
+}
+if ($isCorrect) {
+    echo 'Вы успешно зарегистрировались!<br>';
+}
