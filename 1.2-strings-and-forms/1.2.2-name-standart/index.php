9387e4a488161f33e035ef8a656553c33974c134
sheerazspb <sheerazspb@gmail.com>
2019-05-31 20:30:37 +0300

    fix 1.2-strings-and-forms/1.2.2-name-standart/index.php

--- 1.2-strings-and-forms/1.2.2-name-standart/index.php
+++ 1.2-strings-and-forms/1.2.2-name-standart/index.php
@@ -2,9 +2,13 @@
 $firstName = $_POST['firstName'];
 $lastName = $_POST['lastName'];
 $middleName = $_POST['middleName'];
-$fullName = $lastName . ' ' . $firstName . ' ' . $middleName;
-$fio = $lastName . ' ' . mb_substr($firstName, 0, 1) . '. ' . mb_substr($middleName, 0, 1) . '.';
-$surnameAndInitials = mb_substr($lastName, 0, 1) . mb_substr($firstName, 0, 1) .  mb_substr($middleName, 0, 1);
-echo "Полное имя: $fullName <br>";
-echo "Фамилия и инициалы: $fio <br>";
-echo "Аббревиатура: $surnameAndInitials <br>";
+
+$fullName = ucfirst($lastName) . ' ' . ucfirst($firstName) . ' ' . ucfirst($middleName);
+$fio = mb_strtoupper(mb_substr($lastName, 0, 1)) . mb_strtoupper(mb_substr($firstName, 0, 1)) . mb_strtoupper(mb_substr
+    ($middleName, 0, 1));
+$surnameAndInitials = ucfirst($lastName) . ' ' . mb_strtoupper(mb_substr($firstName, 0, 1)) . '.' . mb_strtoupper
+(mb_substr
+    ($middleName, 0, 1)) . '.';
+echo "Полное имя: '$fullName' <br>"; 
+echo "Фамилия и инициалы: '$fio'  <br>";
+echo "Аббревиатура: '$surnameAndInitials' <br>";
