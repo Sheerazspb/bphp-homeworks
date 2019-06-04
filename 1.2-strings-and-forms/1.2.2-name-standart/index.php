a9bcbda5662f4660d9750987a1d9d7eec6e48129
sheerazspb <sheerazspb@gmail.com>
2019-06-04 15:28:15 +0300

    fix  1.2-strings-and-forms/1.2.2-name-standart/index.php file

--- 1.2-strings-and-forms/1.2.2-name-standart/index.php
+++ 1.2-strings-and-forms/1.2.2-name-standart/index.php
@@ -1,14 +1,17 @@
 <?php
+function mb_ucfirst($string, $encoding) {
+    $strlen = mb_strlen($string, $encoding);
+    $firstChar = mb_substr($string, 0, 1, $encoding);
+    $then = mb_substr($string, 1, $strlen - 1, $encoding);
+    return mb_strtoupper($firstChar, $encoding) . $then;
+}
 $firstName = $_POST['firstName'];
 $lastName = $_POST['lastName'];
 $middleName = $_POST['middleName'];
-
-$fullName = ucfirst($lastName) . ' ' . ucfirst($firstName) . ' ' . ucfirst($middleName);
-$fio = mb_strtoupper(mb_substr($lastName, 0, 1)) . mb_strtoupper(mb_substr($firstName, 0, 1)) . mb_strtoupper(mb_substr
-    ($middleName, 0, 1));
-$surnameAndInitials = ucfirst($lastName) . ' ' . mb_strtoupper(mb_substr($firstName, 0, 1)) . '.' . mb_strtoupper
-(mb_substr
-    ($middleName, 0, 1)) . '.';
-echo "Полное имя: '$fullName' <br>"; 
-echo "Фамилия и инициалы: '$fio'  <br>";
-echo "Аббревиатура: '$surnameAndInitials' <br>";
+$fullName = mb_ucfirst($lastName,'utf-8') . ' ' . mb_ucfirst($firstName,'utf-8') . ' ' . mb_ucfirst($middleName,'utf-8');
+$fio = mb_ucfirst(mb_substr($lastName, 0, 1),'utf-8') . mb_ucfirst(mb_substr($firstName, 0, 1),'utf-8') . mb_ucfirst(mb_substr($middleName, 0, 1),'utf-8');
+$surnameAndInitials = mb_ucfirst($lastName,'utf-8') . ' ' . mb_ucfirst(mb_substr($firstName, 0, 1),'utf-8') . '.' . mb_ucfirst(mb_substr($middleName, 0, 1),'utf-8') . '.' ;
+echo "<div>Полное имя: '$fullName' </div>
+<div>Фамилия и инициалы: '$surnameAndInitials'</div>
+<div>Аббревиатура: '$fio'</div>";
+?>
