41662a18394d41e9a7cc334d242c718bac9cd1f0
sheerazspb <sheerazspb@gmail.com>
2019-06-10 06:19:04 +0300

    updated 2.3-dates-and-sessions/2.3.2-brute-protection/index.php file

--- 2.3-dates-and-sessions/2.3.2-brute-protection/index.php
+++ 2.3-dates-and-sessions/2.3.2-brute-protection/index.php
@@ -1,6 +1,33 @@
 <?php
+session_start();
 $users = [
     'admin' => 'admin1234',
     'randomUser' => 'somePassword',
     'janitor' => 'nimbus2000'
 ];
+$login = $_POST["login"];
+$pass = $_POST["password"];
+$ip = '192.168.1.0';
+foreach ($users as $key => $value) {
+    if ($key === $login && $pass === $value) {
+        echo "you are logged in";
+        return true;
+    }
+}
+$currentTime = time();
+$timeFromCookies = $_COOKIE["date"];
+if ($currentTime - $timeFromCookies < 5) {
+    $date = date('d.m.Y H:i:s');
+    $file = fopen("${login}", 'a');
+    fwrite($file, "${date} : ${$ip} \n");
+    fclose($file);
+    echo "too many attemts try again later after one minut";
+    return false;
+} else {
+    setcookie("date", time());
+    setcookie("ip", $ip);
+    $_SESSION["date"] = time();
+    $_SESSION["ip"] = $ip;
+    echo "incorecct password or login";
+    return false;
+}
