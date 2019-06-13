92a606e0327e4f959c984dcf479a14752c4b2f8d
sheerazspb <sheerazspb@gmail.com>
2019-06-14 01:17:45 +0300

    updated index.php

--- index.php
+++ index.php
@@ -15,23 +15,34 @@
                     <li><a href="1.1-intro-and-branching/1.1.2">Задание 1.1.2</a></li>
                 </ul>
             </li>
-			<li>Занятие 1.2. Работа с формами и строками
+            <li> Занятие 1.2. Формы и строки
                 <ul>
-                    <li><a href="1.2-strings-and-forms/1.2.1-registration/form.html">Задание 1.2.1</a></li>
-                    <li><a href="1.2-strings-and-forms/1.2.2-name-standart/form.html">Задание 1.2.2</a></li>
-                    <li><a href="1.2-strings-and-forms/1.2.3-additional">Задание 1.2.3 Дополнительное</a></li>
+                    <li><a href="1.2-strings-and-forms/1.2.1-registration">Задание 1.2.1</a></li>
+                    <li><a href="1.2-strings-and-forms/1.2.2-name-standart">Задание 1.2.2</a></li>
                 </ul>
             </li>
-			<li>Занятие 1.3. Массивы и циклы
+            <li> Занятие 1.3. Массивы и циклы
                 <ul>
-                    <li><a href="1.3-arrays-and-functions/1-places">Задание 1.3.1</a></li>
-                    <li><a href="1.3-arrays-and-functions/2-search">Задание 1.3.2</a></li>
+                    <li><a href="1.3-arrays-and-functions/1-places">Задание 1</a></li>
+                    <li><a href="1.3-arrays-and-functions/2-search">Задание 2</a></li>
                 </ul>
             </li>
-			<li>Занятие 2.1. Файлы
+            <li> Занятие 2.1. Файлы
                 <ul>
-                    <li><a href="2.1-files/2.1.1/csvtojson.php">Задание 2.1.1</a></li>
-                    <li><a href="2.1-files/2.1.2">Задание 2.1.2</a></li>
+                    <li><a href="2.1-files/2.1.1">Задание 1</a></li>
+                    <li><a href="2.1-files/2.1.2">Задание 2</a></li>
+                </ul>
+            </li>
+            <li> Введение в ООП 2.2.
+                <ul>
+                    <li><a href="2.2-OOP/2.2.1">Задание 1</a></li>
+                    <li><a href="2.1-OOP/2.2.2">Задание 2</a></li>
+                </ul>
+            </li>
+            <li> Занятие 2.3. dates-and-sessions.
+                <ul>
+                    <li><a href="2.3-dates-and-sessions/2.3.1-your-tube/">Задание 1</a></li>
+                    <li><a href="2.3-dates-and-sessions/2.3.2-brute-protection/form.html">Задание 2</a></li>
                 </ul>
             </li>
         </ul>
