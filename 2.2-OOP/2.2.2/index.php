1fa5bf09b0e2b2ac748012a15a7de1345793f335
sheerazspb <sheerazspb@gmail.com>
2019-07-02 08:24:57 +0300

    updated 2.2-OOP/2.2.2/index.php

---
+++ 2.2-OOP/2.2.2/index.php
@@ -0,0 +1,8 @@
+<?php
+include 'autoload.php';
+include 'config/SystemConfig.php';
+include 'classes/JsonFileAccessModel.php';
+$jsonObj = new JsonFileAccessModel('users');
+$json = $jsonObj->readJson();
+var_dump($json);
+?>
