584130717697a162f3260c93fd02ade37b69ccac
sheerazspb <sheerazspb@gmail.com>
2019-07-31 04:39:00 +0300

    updated 2.3-dates-and-sessions/2.3.2-brute-protection/index.php

--- 2.3-dates-and-sessions/2.3.2-brute-protection/index.php
+++ 2.3-dates-and-sessions/2.3.2-brute-protection/index.php
@@ -1,33 +1,52 @@
 <?php
+
 session_start();
+
 $users = [
     'admin' => 'admin1234',
     'randomUser' => 'somePassword',
     'janitor' => 'nimbus2000'
 ];
-$login = $_POST["login"];
-$pass = $_POST["password"];
-$ip = '192.168.1.0';
-foreach ($users as $key => $value) {
-    if ($key === $login && $pass === $value) {
-        echo "you are logged in";
+
+if(isset($_POST['submit'])) {
+
+    if (!isset($_SESSION['counter'])) {
+        $_SESSION['counter'] = 0;
+    };    
+
+    $login = $_POST['login'];
+    $pass = $_POST['password'];
+
+    if(array_key_exists($login, $users) && $users[$login] === $pass) {
+
+        echo 'You are successfully logged in!';
         return true;
+        
+    } else {
+
+        $_SESSION['counter'] += 1;
+
+        setcookie('error[' . $_SESSION['counter'] . ']', time());
+
+        if (isset($_COOKIE['error'])) {
+
+            if ((time() - $_COOKIE['error'][$_SESSION['counter'] - 1]) < 5 || (time() - $_COOKIE['error'][$_SESSION['counter'] - 2]) < 60) {
+
+                $date = date("j F Y, g:i a");
+                $file = fopen("${login}", 'a');
+                fwrite($file, "${date} \n");
+                fclose($file);
+
+                echo 'too many attempts try again later after one minut!';
+                return false;
+
+            } else {
+                setcookie("date", time());
+                $_SESSION["date"] = time();
+                echo 'incorecct password or login!';
+                return false;
+            }
+        }
     }
 }
-$currentTime = time();
-$timeFromCookies = $_COOKIE["date"];
-if ($currentTime - $timeFromCookies < 5) {
-    $date = date('d.m.Y H:i:s');
-    $file = fopen("${login}", 'a');
-    fwrite($file, "${date} : ${$ip} \n");
-    fclose($file);
-    echo "too many attempts try again later after one minut";
-    return false;
-} else {
-    setcookie("date", time());
-    setcookie("ip", $ip);
-    $_SESSION["date"] = time();
-    $_SESSION["ip"] = $ip;
-    echo "incorecct password or login";
-    return false;
-}
+ 
