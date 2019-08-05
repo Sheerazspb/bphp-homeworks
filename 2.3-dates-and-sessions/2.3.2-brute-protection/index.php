17fd23e3e7636acafc25f52df20ee9f6d8f52f29
sheerazspb <sheerazspb@gmail.com>
2019-08-05 23:51:45 +0300

    fix

--- 2.3-dates-and-sessions/2.3.2-brute-protection/index.php
+++ 2.3-dates-and-sessions/2.3.2-brute-protection/index.php
@@ -1,52 +1,78 @@
 <?php
-
-session_start();
-
 $users = [
     'admin' => 'admin1234',
     'randomUser' => 'somePassword',
     'janitor' => 'nimbus2000'
 ];
 
-if(isset($_POST['submit'])) {
-
-    if (!isset($_SESSION['counter'])) {
-        $_SESSION['counter'] = 0;
-    };    
-
-    $login = $_POST['login'];
-    $pass = $_POST['password'];
-
-    if(array_key_exists($login, $users) && $users[$login] === $pass) {
-
-        echo 'You are successfully logged in!';
-        return true;
-        
-    } else {
-
-        $_SESSION['counter'] += 1;
-
-        setcookie('error[' . $_SESSION['counter'] . ']', time());
-
-        if (isset($_COOKIE['error'])) {
-
-            if ((time() - $_COOKIE['error'][$_SESSION['counter'] - 1]) < 5 || (time() - $_COOKIE['error'][$_SESSION['counter'] - 2]) < 60) {
-
-                $date = date("j F Y, g:i a");
-                $file = fopen("${login}", 'a');
-                fwrite($file, "${date} \n");
-                fclose($file);
-
-                echo 'too many attempts try again later after one minut!';
-                return false;
-
-            } else {
-                setcookie("date", time());
-                $_SESSION["date"] = time();
-                echo 'incorecct password or login!';
-                return false;
-            }
+function login($users) {
+    if (array_key_exists($_POST['login'], $users)) {
+        if ($users[$_POST['login']] === $_POST['password']) {
+            echo 'You are successfully logged in!';
+            return true;
         }
+    }
+    echo 'incorecct password or login!';
+    return false;
+}
+
+function check($users) {
+    if (login($users) === true) {
+        exit;
+    } else {
+        $_SESSION['login'] = $_POST['login'];
+        $_SESSION['time'] = time();
+        $_SESSION['counter'] = 1;
+        return;
+    }
+}
+
+function usersAttempts($users) {
+    session_set_cookie_params(1800);
+    session_start();
+ 
+    if (count($_SESSION) == 0) {
+        check($users);
+        return;
+    }
+ 
+    if ($_SESSION['login'] === $_POST['login']) {
+        
+        $_SESSION['counter'] += 1;
+        
+        if (((time() - $_SESSION['time']) <= 5) && ($_SESSION['counter'] === 2)) {
+            
+            echo 'too many attempts try again later after one minut!';
+            
+            $file = 'data.txt';
+            $userFile = fopen($file, 'a');
+            $date = $_POST['login'] . ': ' . date('d.m.Y H:i:s') . "\n";
+            fwrite($userFile, $date);
+            fclose($fpFile);
+            
+        } elseif ((time() - $_SESSION['time']) < 30) {
+            $_SESSION['counter']++;
+            if ($_SESSION['counter'] > 3) {
+                $_SESSION['counter'] = 0;
+                echo 'too many attempts try again later after one minut!';
+                
+                $file = 'data.txt';
+                $userFile = fopen($file, 'a');
+                $date = $_POST['login'] . ': ' . date('d.m.Y H:i:s') . "\n";
+                fwrite($userFile, $date);
+                fclose($fpFile);
+            }
+ 
+        } elseif ((time() - $_SESSION['time']) > 30) {
+            $_SESSION = [];
+            check($users);
+            return;
+        }
+ 
+    } else {
+        check($users);
+        return;
     }
 }
  
+usersAttempts($users);
